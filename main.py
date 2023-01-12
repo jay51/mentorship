cda6e4570cd32548c6275178d7c67f2f3bb46636
jay51 <nenojafaar@gmail.com>
2023-01-11 20:59:51 -0600

    calculator program

--- main.py
+++ main.py
@@ -1,19 +1,101 @@
 
 
-# argv = ['jamal', 'jack', 'nick']
+def ask_user():
+    num1 = int(input("First Number >: "))
+    num2 = int(input("Second Number >: "))
 
-# for name in argv:
-#   print("Hello " + name)
+    result = [num1, num2]
+    return result
 
-# argv = ['jamal', 'jack', 'nick']
-# print("Hello" + argv[0])
-# print("Hello" + argv[1])
-# print("Hello" + argv[])
+def write_operation(op, num1, num2, result):
 
-names = ['jamal', 'jack', 'nick']
-length = len(names) # 3
-x = 0
+  string = f"operation: {op} {num1}, {num2}\n"
+  rs = f"result:  {result}\n"
 
-while x < length:
-  print("name at index " + str(x) + " = " + names[x])
-  x = x + 1
+  file.write(string)
+  file.write(rs)
+  file.flush()
+
+
+file = open("./math", "w")
+
+# operation = 'ADD'
+# num1 = 4
+# num2 = 4
+
+# string = "opertion: " + operation + str(num1) + ", "  + str(num2) + "\n"
+# file.write(string)
+# file.write("result: 3\n")
+
+# file.write("opertion: Sub 5, 4\n")
+# file.write("result: 1\n")
+
+while True:
+  print("""
+  1- Add numbers
+  2- Sub numbers
+  3- Mul numbers
+  4- exit
+  
+  """)
+
+  value = input(">: ")
+
+  if value == "1":
+    numbers = ask_user()
+    result = numbers[0] + numbers[1]
+    print("result >: ", result )
+
+    # wite to file
+    # > oporation: ADD 1, 2
+    # > result: 4
+
+    # string = "operation: ADD " +  str(numbers[0])  + ", "  + str(numbers[1]) + "\n"
+    # rs = "result: " + str(result) + "\n"
+
+    # string = f"operation: ADD  {numbers[0]}, {numbers[1]}\n"
+    # rs = f"result:  {result}\n"
+
+    # file.write(string)
+    # file.write(rs)
+    # file.flush()
+    write_operation("ADD", numbers[0], numbers[1], result)
+
+
+  if value == "2":
+    numbers = ask_user()
+    result = numbers[0] - numbers[1]
+    print("result >: ", result )
+
+    # wite to file
+    # > oporation: Sub 2 2
+    # > result: 0
+    # string = f"operation: SUB {numbers[0]}, {numbers[1]} \n"
+    # rs = f"result : {result}\n"
+
+    # file.write(string)
+    # file.write(rs)
+    # file.flush()
+    write_operation("SUB", numbers[0], numbers[1], result)
+
+  if value == "3":
+    numbers = ask_user()
+    result = numbers[0] * numbers[1]
+    print("result >: ", result )
+
+    # wite to file
+    # > oporation: Mul 2 2
+    # > result: 4
+
+    # string = f"Operation: Mul {numbers[0]} , {numbers[1]}\n"
+    # rs = f"result: {result}\n"
+
+    # file.write(string)
+    # file.write(rs)
+    # file.flush()
+    write_operation("MUL", numbers[0], numbers[1], result)
+ 
+
+  if value == "4":
+    break
+  dont forget to 
