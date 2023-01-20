a096bde2ff2591d9543e3f19119edc1ce5aaaf36
jay51 <nenojafaar@gmail.com>
2023-01-19 20:48:02 -0600

    working with files

--- reader.py
+++ reader.py
@@ -23,28 +23,106 @@
 
 # file.close
     
+3
+def get_all_users_except(id):
+    file = open("./passwords.csv", "r")
+    lines = file.readlines()
+# [
+# 'id,name,password',
+# '1,jamal,123456',
+# '2,jay,126kdjsadk',
+
+# '4,nick,123456jskdjf',
+# '5,jurgen,123456kajsdf',
+# ]
+    result = []
+    for line in lines:
+
+        # ['1', 'jamal' ,'123456']
+        values = line.split(',')
+        if values[0] != id:
+            result.append(line)
+
+    file.close()
+    return result
+
+
+def find_user(name):
+    file = open("./passwords.csv", "r")
+    lines = file.readlines()
+# [
+# 'id,name,password',
+# '1,jamal,123456',
+# '2,jay,126kdjsadk',
+# '3,jack,123456jsdkfj',
+# '4,nick,123456jskdjf',
+# '5,jurgen,123456kajsdf',
+# ]
+    result = []
+    for line in lines:
+
+        # ['1', 'jamal' ,'123456']
+        values = line.split(',')
+        if values[1] == name:
+            result.append(line)
+
+    file.close()
+    return result
+
+        # if name in line:
+        #     pass
+
+
+def add_user(id, name, password):
+    file = open("./passwords.csv", "a")
+    combo = f"{id},{name},{password}\n"
+    file.write(combo)
+    file.flush()
+    file.close()
 
 
 
+file = open("./passwords.csv", "r")
+id = len(file.readlines())
+file.close
 
 while True:
   
     print("""
     1- search User
     2- Add User
-    3- exit
+    3- Delet User
+    4- exit
 
     """)
 
     value = input(">: ")
 
     if value == "1":
-        print('search user')
+        name = input("User Name: ")
+        result = find_user(name) # ['1,jamal, 123456', '2,jamal,54321']
+        for line in result:
+            print(line)
+
 
     elif value == "2":
-        print('add user')
+        ask_name = input("What is your name?")
+        ask_password = input("What is your password?")
+        add_user(id, ask_name, ask_password)
+        id +=1 
 
     elif value == "3":
+        user_id = input("user_id: ")
+        users = get_all_users_except(user_id)
+
+        file = open('./passwords.csv', 'w')
+        for user in users:
+            file.write(user)
+            file.flush()
+        
+        file.close()
+    
+    elif value == "4":
         break
     else:
         print("invalid optin")
